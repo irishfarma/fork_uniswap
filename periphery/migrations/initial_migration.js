9a62fc1c22df5c6cc306f007e13e5e4d62db1d8f
Luka Pavageau <lukapavageau@MacBook-Air.local>
2023-07-21 23:26:57 +0200

    First commit

---
+++ periphery/migrations/initial_migration.js
@@ -0,0 +1,20 @@
+var Router = artifacts.require("UniswapV2Router02.sol");
+var WETH = artifacts.require("WETH.sol");
+
+module.exports = async function(deployer) {
+    let weth;
+    const FACTORY_ADDRESS = '0xE18ed0beD68617Bd4446Ce7A72fdc4e2a5Dae8DF';
+  // deployment steps
+
+
+
+  if(network === 'mainnet'){
+    weth = await WETH.at('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
+  } else {
+
+    await deployer.deploy(WETH);
+    weth = await WETH.deployed();
+  }
+  await deployer.deploy(Router, FACTORY_ADDRESS, weth.address);
+
+};
